15f021d0ea440356edfefc163c5c96717fa09da5
hackformission <46250194+hackformission@users.noreply.github.com>
2018-12-30 17:26:03 -0500

    Add files via upload
    bat file to install python dependencies

---
+++ pipInstalls.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo installing mysql.connector (1/4)
+pip install mysql.connector
+
+echo tornado (2/4)
+pip install tornado
+
+echo APScheduler (3/4)
+pip install APScheduler
+
+echo numpy (4/4)
+pip install numpy
+
+echo Installation Complete!
