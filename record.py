68e99524eb1a65801e3ca256b4e29e146d1c71c3
Matt Schlosser <websitemakingguy@gmail.com>
2018-12-30 13:28:58 -0700

    Updates to comments

--- record.py
+++ record.py
@@ -3,8 +3,9 @@ import re
 
 
 def record(station, port, freq, mode, time):
-    """Record takes a string station (web address), an integer port, an integer freq, a string mode, and a integer time
-     in seconds, and returns the RSSI data of the recording being saved, as a list of floats.
+    """Record takes a string station (web address), an integer port, an integer freq, the frequency, in kHz,
+    a string mode (e.g. am), and a integer time, in seconds for the length of the recording.
+    It returns the RSSI data of the recording being saved, as a list of floats.
     The recording is saved in the directory that the script is run on under the default file name,
      which has the format (YYYYMMDDTHHMMSSZ_station_mode).wav"""
     command = ["python", "Kiwi/kiwirecorder.py", "-k", str(30), "-s", str(station), "-p", str(port), "-f", str(freq), "-m", mode,
@@ -15,4 +16,4 @@ def record(station, port, freq, mode, time):
     return [float(i) for i in d]
 
 
-data = record("hat2018.twrmon.net", 8075, 800, "am", 10);
+data = record("hat2018.twrmon.net", 8075, 800, "am", 10)
