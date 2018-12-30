792f7d37364dac6d6dddf0107a58faf0cd0cbb5e
william zhu <william_zhu@hotmail.ca>
2018-12-30 16:48:38 -0500

    finished scheduler

--- Kiwi/scheduler.py
+++ Kiwi/scheduler.py
@@ -3,26 +3,43 @@ Demonstrates how to schedule a job to be run in a process pool on 3 second inter
 """
 # from kiwirecorder import kiwirecorder
 from datetime import datetime
-from record import record
+from record import record as rssi_record
 
 import logging
 import os
 
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 
-def information():
-    record("hat2018.twrmon.net", 8075, 800, "am", 10)
+class Scheduler:
+    def __init__(self):
+        # intervals = input('how many seconds?')
+        logger.info("start")
+        self.scheduler = BackgroundScheduler(logger=logger)
+        # scheduler.add_job(information, 'interval', seconds=15)
+        print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+        self.scheduler.start()
+
+    def addJob(self, params):
+        self.scheduler.add_job(self._record, 'date', run_date=params['time'], args=[params])
+        
+    def _record(self, params):
+        rssi_record(params['station'], params['port'], params['freq'], params['mode'], params['duration'], params['schedule_id'])
+
+    def stop(self):
+        self.scheduler.shutdown()
 
 if __name__ == '__main__':
-    # intervals = input('how many seconds?')
-    scheduler = BlockingScheduler(logger=logger)
-    scheduler.add_job(information, 'interval', seconds=15)
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    scheduler = Scheduler()
 
-    try:
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        pass
+    from datetime import datetime, timedelta
+    import time as t
+    time = datetime.now() + timedelta(seconds=10)   
+
+    scheduler.addJob({ "station": "hat2018.twrmon.net", "port": 8075, "freq": 800, "mode": "am", "time": time, "schedule_id": 31, "duration": 10})
+
+    t.sleep(15)
+    scheduler.stop()
